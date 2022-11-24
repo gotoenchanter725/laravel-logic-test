1f08fe13e99d3849ca016866b5b49a90f30dd9d9
HarryPotter0308 <creativemoderndev0831>
2022-11-25 05:06:05 +0900

    Update show

--- resources/views/pages/admin/users/show.blade.php
+++ resources/views/pages/admin/users/show.blade.php
@@ -5,28 +5,43 @@
 @section('content')
     <div class="flex flex-col items-center justify-center min-h-screen bg-primary p-8">
         <div class="px-8 py-6 mx-4 mt-4 text-left bg-white shadow-lg w-full flex flex-col">
-            <div class="row">
-                <div class="col-lg-12 margin-tb">
-                    <div class="pull-left">
-                        <h2> Show User</h2>
-                    </div>
-                    <div class="pull-right">
-                        <a class="btn btn-primary" href="{{ route('admin.users.index') }}"> Back</a>
-                    </div>
-                </div>
+            <div class="flex justify-between items-center mb-4">
+                <h2 class="text-2xl font-bold">Show User Detail</h2>
+                <a class="px-6 py-2 mt-4 text-white bg-primary rounded-lg hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-gray-300"
+                    href="{{ route('admin.users.index') }}"> Back</a>
             </div>
 
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Name:</strong>
+            <div class="flex flex-col">
+                <div class="flex items-center mb-2">
+                    <div class="min-w-[200px] font-semibold text-lg">
+                        Name:
+                    </div>
+                    <div>
                         {{ $user->name }}
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Phone Number:</strong>
+                <div class="flex items-center mb-2">
+                    <div class="min-w-[200px] font-semibold text-lg">
+                        Phone Number:
+                    </div>
+                    <div>
                         {{ $user->phone }}
+                    </div>
+                </div>
+                <div class="flex items-center mb-2">
+                    <div class="min-w-[200px] font-semibold text-lg">
+                        Access Link:
+                    </div>
+                    <div class="overflow-hidden">
+                        {{ route('base_url') . '/check/' . $user->link }}
+                    </div>
+                </div>
+                <div class="flex items-center mb-2">
+                    <div class="min-w-[200px] font-semibold text-lg">
+                        Expires DateTime:
+                    </div>
+                    <div class="overflow-hidden">
+                        {{ $user->expires_at }}
                     </div>
                 </div>
             </div>
